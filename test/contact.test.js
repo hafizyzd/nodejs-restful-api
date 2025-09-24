4108cb710ea610290727b0aa328ae3658591c68a
Hafiz Yazid Muhammad <hafizyazidmuhammad@gmail.com>
2025-09-24 20:20:41 +0700

    delete

--- test/contact.test.js
+++ test/contact.test.js
@@ -232,9 +232,20 @@ describe('DELETE /api/contacts/:contactId', function (){
 
     it('should can delete contact', async () => {
         let testContact = await getTestContact();
+
+        const loginResult = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "rahasia"
+            });
+        
+        expect(loginResult.status).toBe(200);
+        expect(loginResult.body.data.token).toBeDefined();
+
         const result = await supertest(web)
             .delete('/api/contacts/' + testContact.id)
-            .set('Authorization', 'test');
+            .set('Authorization', loginResult.body.data.token);
 
         expect(result.status).toBe(200);
         expect(result.body.data).toBe("OK");
@@ -243,14 +254,25 @@ describe('DELETE /api/contacts/:contactId', function (){
         expect(testContact).toBeNull();
     });
 
-    // it('should reject if contact is not found', async () => {
-    //     let testContact = await getTestContact();
-    //     const result = await supertest(web)
-    //         .delete('/api/contacts/' + (testContact.id + 1))
-    //         .set('Authorization', 'test');
+    it('should reject if contact is not found', async () => {
+        let testContact = await getTestContact();
+        
+        const loginResult = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "rahasia"
+            });
+        
+        expect(loginResult.status).toBe(200);
+        expect(loginResult.body.data.token).toBeDefined();
 
-    //     expect(result.status).toBe(404);
-    // });
+        const result = await supertest(web)
+            .delete('/api/contacts/' + (testContact.id + 1))
+            .set('Authorization', loginResult.body.data.token);
+
+        expect(result.status).toBe(404);
+    });
 });
 
 
